0eae0a91a60c3e6c0dde02f88d20c94f7c76cfe8
DimitarSeykov <50461989+DimitarSeykov@users.noreply.github.com>
2020-11-12 21:15:41 +0200

    iterative repair plus random restart

--- 02/N-Queens/N-Queens/N-Queens.cpp
+++ 02/N-Queens/N-Queens/N-Queens.cpp
@@ -2,13 +2,19 @@
 #include <time.h>
 #include <cstdlib>
 #include <vector>
-#include <limits>
+#include <limits.h>
+#include <utility>
+#include <chrono>
+
 
 using namespace std;
 
-const int BOARD_SIDE = 5;
+const int BOARD_SIDE = 100;
 
 typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+
+const int MOVES_FOR_RESTART = 70;
 
 template <class T>
 void printVector(vector<T>& v) {
@@ -32,7 +38,7 @@ void printMatrix(vector<vector<T>>& v) {
 class Game {
 public:
     void generateInitialBoard() {
-        
+
         initSizes();
 
         for (int i = 0; i < BOARD_SIDE; i++) {
@@ -41,9 +47,7 @@ public:
             vector<int> conflitsCurrentRow(BOARD_SIDE);
             int minConflicts = INT_MAX;
             for (int j = 0; j < BOARD_SIDE; j++) {
-                int conflicts = conflictsColumns[j] +
-                                conflictsMainDiag[getMainDiag(i, j)] +
-                                conflictsSecDiag[getSecDiag(i, j)];
+                int conflicts = getConflictsAtPosition(i, j);
                 conflitsCurrentRow[j] = conflicts;
                 if (conflicts < minConflicts) {
                     minConflicts = conflicts;
@@ -61,12 +65,12 @@ public:
 
             queens[i] = pos;
             board[i][pos] = 1;
-            
-            conflictsColumns[pos]++;
-            conflictsMainDiag[getMainDiag(i, pos)]++;
-            conflictsSecDiag[getSecDiag(i, pos)]++;
-            //printMatrix(board);
+
+            updateConflicts(i, pos, 1);
         }
+
+        // printMatrix(board);
+        // cout << getTotalConflicts() << endl;
 
         // printVector(queens);
         // printVector(conflictsColumns);
@@ -74,17 +78,81 @@ public:
         // printVector(conflictsSecDiag);
     }
 
-    int getConflicts() {
-
+    int getTotalConflicts() {
+        return getConflictFromDirecton(conflictsMainDiag) +
+               getConflictFromDirecton(conflictsSecDiag) +
+               getConflictFromDirecton(conflictsColumns);
     }
-    
+
+    void makeMove() {
+        int maxConflicts = -1;
+        vector<int> candidates;
+        for (int i = 0; i < queens.size(); i++) {
+            int currentConflicts = getConflictsAtPosition(i, queens[i]);
+            if (currentConflicts > maxConflicts) {
+                maxConflicts = currentConflicts;
+                if (currentConflicts > 3) { // because of 1s in conflict arrays
+                    candidates.push_back(i);
+                }
+            }
+        }
+
+        int worstQueenIdx = candidates[rand() % candidates.size() + 0];
+
+        int minConflicts = INT_MAX;
+        vector<int> moveCandidates;
+        for (int i = 0; i < BOARD_SIDE; i++) {
+            int currentConflicts = getConflictsAtPosition(worstQueenIdx, i);
+            if (currentConflicts < minConflicts) {
+                minConflicts = currentConflicts;
+                moveCandidates.push_back(i);
+            }
+        }
+
+
+        int movePos = moveCandidates[rand() % moveCandidates.size() + 0];
+        // cout << "worstQueen: " << worstQueenIdx << endl;
+        // cout << "move position: " << movePos << endl;
+
+        updateConflicts(worstQueenIdx, queens[worstQueenIdx], -1);
+        updateConflicts(worstQueenIdx, movePos, 1);
+
+        swap(board[worstQueenIdx][queens[worstQueenIdx]], board[worstQueenIdx][movePos]);
+        queens[worstQueenIdx] = movePos;
+
+        // cout << "conflicts: " << getTotalConflicts() << endl;
+        //printMatrix(board);
+    }
+
+    void solveRandomRestart(){
+        int moves = 0;
+        int restarts = 0;
+        while (getTotalConflicts() > 0) {
+            if (moves > MOVES_FOR_RESTART) {
+                generateInitialBoard();
+                restarts++;
+                moves = 0;
+                continue;
+            }
+            // cout << "moves " << moves << endl;
+            makeMove();
+            //printMatrix(game.board);
+            moves++;
+        }
+        cout << "moves: " << moves << endl;
+        cout << "restarts: " << restarts << endl;
+        if(BOARD_SIDE <= 15){
+            printMatrix(board);
+        }
+    }
+
+
+private:
     vvi board;
     vector<int> queens;
     vector<int> conflictsMainDiag;
     vector<int> conflictsSecDiag;
     vector<int> conflictsColumns;
-
-private:
 
     int getMainDiag(int x, int y) {
         return (BOARD_SIDE - 1 - x + y);
@@ -92,6 +160,12 @@ private:
 
     int getSecDiag(int x, int y) {
         return x + y;
+    }
+
+    int getConflictsAtPosition(int x, int y) {
+        return conflictsColumns[y] +
+               conflictsMainDiag[getMainDiag(x, y)] +
+               conflictsSecDiag[getSecDiag(x, y)];
     }
 
     void initSizes() {
@@ -105,14 +179,39 @@ private:
         conflictsMainDiag.assign(2 * BOARD_SIDE - 1, 0);
         conflictsSecDiag.assign(2 * BOARD_SIDE - 1, 0);
     }
+
+    int getConflictFromDirecton(vector<int>& v) {
+        int sum = 0;
+        for (int conf : v) {
+            if (conf > 1) {
+                sum += (conf - 1);
+            }
+        }
+
+        return sum;
+    }
+
+    void updateConflicts(int x, int y, int factor) {
+        conflictsColumns[y] += factor;
+        conflictsMainDiag[getMainDiag(x, y)] += factor;
+        conflictsSecDiag[getSecDiag(x, y)] += factor;
+    }
 };
 
 int main()
 {
     srand(time(nullptr));
-    
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
     Game game;
     game.generateInitialBoard();
-    printMatrix(game.board);
+    game.solveRandomRestart();
+
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "[ms]" << endl;
 }
 
